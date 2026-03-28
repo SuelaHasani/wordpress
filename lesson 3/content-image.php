807f4b0345e4de3dfdfd73466a5d71c93fd7bf26
SuelaHasani <suelahasani206@gmail.com>
2026-03-28 14:13:01 +0100

    lesson 3

---
+++ lesson 3/content-image.php
@@ -0,0 +1,6 @@
+<article <?php post_class();?>>
+
+<h2> <?php the_title();?></h2>
+<?php the_content();?>
+
+</article>
---
+++ lesson 3/page-ds-protofolio.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+
+<h1> ds protofolio test test</h1>
+
+<?php if (have_posts()) : while(have_posts()) :the_post(); ?>
+  <?php the_content();?>
+
+<?php endwhile; endif;?>
+
+<?php get_footer();?>
